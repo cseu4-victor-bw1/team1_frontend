dc53ae260325776a2b0d7f9402d146eab995b297
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2020-04-27 14:38:27 +0100

    basic routing and files in place

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Login from "./components/login";
+import Home from "./components/home";
+import Dashboard from "./components/dashboard";
+import Nav from "./components/nav";
+import Footer from "./components/footer"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Footer />
     </div>
   );
 }
